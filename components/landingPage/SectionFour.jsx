a6e7dd6e7f8715b9e3c3d6dbb8c5bfdf407d70ae
MAC <mac@MacBook-Pro-de-MAC.local>
2024-08-03 18:05:33 +0100

    use client

--- components/landingPage/SectionFour.jsx
+++ components/landingPage/SectionFour.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState } from 'react';
 import { useTranslations, useLocale } from 'next-intl';
 import Image from 'next/image';
--- components/landingPage/SectionTwo.jsx
+++ components/landingPage/SectionTwo.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 import Image from 'next/image';
